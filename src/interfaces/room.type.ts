89f4523b5ccab857888914f11a7a5cff8d60a7b0
bmancilla23 <bmancilla119@gmail.com>
2024-05-12 23:04:08 -0500

    feat: cretead room type

---
+++ src/interfaces/room.type.ts
@@ -0,0 +1,19 @@
+import { ReactElement } from 'react';
+
+// Definir las interfaces
+export interface Facility {
+  name: string;
+  icon: ReactElement;
+}
+
+export interface Room {
+  id: number;
+  name: string;
+  description: string;
+  facilities: Facility[];
+  size: number;
+  maxPerson: number;
+  price: number;
+  image: string;
+  imageLg: string;
+}
